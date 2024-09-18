8ec0c6ada0ea9681ecbaa84190715263fa235367
xytyfresch <161212936+xytyfresch@users.noreply.github.com>
2024-09-18 10:02:08 +0300

    put magic numbers into constants

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import TaskColumnComponent from "./view/task-column-component.js";
 import TaskComponent from "./view/task-component.js";
 import { render, RenderPosition } from "./framework/render.js";
 
+const numberOfStatuses = 4;
+const numberOfTasks = 4;
+
 const bodyContainer = document.querySelector("body");
 const formContainer = document.querySelector(".task-form");
 const taskBoardContainer = document.querySelector(".task-board");
@@ -11,13 +14,13 @@ const taskBoardContainer = document.querySelector(".task-board");
 render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
 render(new FormAddTaskComponent(), formContainer);
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < numberOfStatuses; i++) {
   const taskColumnComponent = new TaskColumnComponent();
   render(taskColumnComponent, taskBoardContainer);
 
   const tasksList = document.querySelectorAll(`.task-list`);
 
-  for (let j = 0; j < 4; j++) {
+  for (let j = 0; j < numberOfTasks; j++) {
     render(new TaskComponent(), tasksList[i]);
   }
 }
